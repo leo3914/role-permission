ff44c5225fa53e0334c1e7f3a1c177574f53b8b2
Pyae Phyo <phyopyae.3914@gmail.com>
2024-11-13 16:38:44 +0630

    response with paginate complete

--- resources/js/components/SideComponent/TestingComponent.vue
+++ resources/js/components/SideComponent/TestingComponent.vue
@@ -1,166 +1,35 @@
 <template>
-    <v-data-table-server
-        v-model:items-per-page="itemsPerPage"
-        :headers="headers"
-        :items="serverItems"
-        :items-length="totalItems"
-        :loading="loading"
-        :search="search"
-        item-value="name"
-        @update:options="loadItems">
-    </v-data-table-server>
+<v-data-table-server v-model:items-per-page="itemsPerPage" :headers="headers" :items="serverItems" :items-length="totalItems" :loading="loading" :search="search" item-value="name" @update:options="loadItems">
+</v-data-table-server>
 </template>
 
-<!-- <script>
-const desserts = [{
-        name: 'Frozen Yogurt',
-        calories: 159,
-        fat: 6.0,
-        carbs: 24,
-        protein: 4.0,
-        iron: '1',
-    },
-    {
-        name: 'Jelly bean',
-        calories: 375,
-        fat: 0.0,
-        carbs: 94,
-        protein: 0.0,
-        iron: '0',
-    },
-    {
-        name: 'KitKat',
-        calories: 518,
-        fat: 26.0,
-        carbs: 65,
-        protein: 7,
-        iron: '6',
-    },
-    {
-        name: 'Eclair',
-        calories: 262,
-        fat: 16.0,
-        carbs: 23,
-        protein: 6.0,
-        iron: '7',
-    },
-    {
-        name: 'Gingerbread',
-        calories: 356,
-        fat: 16.0,
-        carbs: 49,
-        protein: 3.9,
-        iron: '16',
-    },
-    {
-        name: 'Ice cream sandwich',
-        calories: 237,
-        fat: 9.0,
-        carbs: 37,
-        protein: 4.3,
-        iron: '1',
-    },
-    {
-        name: 'Lollipop',
-        calories: 392,
-        fat: 0.2,
-        carbs: 98,
-        protein: 0,
-        iron: '2',
-    },
-    {
-        name: 'Cupcake',
-        calories: 305,
-        fat: 3.7,
-        carbs: 67,
-        protein: 4.3,
-        iron: '8',
-    },
-    {
-        name: 'Honeycomb',
-        calories: 408,
-        fat: 3.2,
-        carbs: 87,
-        protein: 6.5,
-        iron: '45',
-    },
-    {
-        name: 'Donut',
-        calories: 452,
-        fat: 25.0,
-        carbs: 51,
-        protein: 4.9,
-        iron: '22',
-    },
-]
-
-const FakeAPI = {
-    async fetch({
-        page,
-        itemsPerPage,
-        sortBy
-    }) {
-        return new Promise(resolve => {
-            setTimeout(() => {
-                const start = (page - 1) * itemsPerPage
-                const end = start + itemsPerPage
-                const items = desserts.slice()
-
-                if (sortBy.length) {
-                    const sortKey = sortBy[0].key
-                    const sortOrder = sortBy[0].order
-                    items.sort((a, b) => {
-                        const aValue = a[sortKey]
-                        const bValue = b[sortKey]
-                        return sortOrder === 'desc' ? bValue - aValue : aValue - bValue
-                    })
-                }
-
-                const paginated = items.slice(start, end)
-
-                resolve({
-                    items: paginated,
-                    total: items.length
-                })
-            }, 500)
-        })
-    },
-}
+<script>
+import axios from 'axios';
 
 export default {
     data: () => ({
         itemsPerPage: 5,
         headers: [{
-                title: 'Dessert (100g serving)',
-                align: 'start',
-                sortable: false,
+                title: 'Name',
                 key: 'name',
+                align: 'start'
             },
             {
-                title: 'Calories',
-                key: 'calories',
-                align: 'end'
+                title: 'Email',
+                key: 'email',
+                align: 'start'
             },
             {
-                title: 'Fat (g)',
-                key: 'fat',
-                align: 'end'
+                title: 'Phone',
+                key: 'phone',
+                align: 'start'
             },
             {
-                title: 'Carbs (g)',
-                key: 'carbs',
-                align: 'end'
+                title: 'role',
+                key: 'role.role',
+                align: 'start'
             },
-            {
-                title: 'Protein (g)',
-                key: 'protein',
-                align: 'end'
-            },
-            {
-                title: 'Iron (%)',
-                key: 'iron',
-                align: 'end'
-            },
+            // { title: 'Website', key: 'website', align: 'start' }
         ],
         search: '',
         serverItems: [],
@@ -170,70 +39,38 @@ export default {
     methods: {
         loadItems({
             page,
-            itemsPerPage,
-            sortBy
+            itemsPerPage
         }) {
-            this.loading = true
-            FakeAPI.fetch({
-                page,
-                itemsPerPage,
-                sortBy
-            }).then(({
-                items,
-                total
-            }) => {
-                this.serverItems = items
-                this.totalItems = total
-                this.loading = false
-            })
-        },
-    },
-}
-</script> -->
-
-<script>
-import axios from 'axios';
-
-    export default {
-        data:() => ({
-        itemsPerPage : 5,
-        headers: [
-            { title: 'Name', key: 'name', align: 'start' },
-            { title: 'Username', key: 'username', align: 'start' },
-            { title: 'Email', key: 'email', align: 'start' },
-            { title: 'Phone', key: 'phone', align: 'start' },
-            { title: 'Website', key: 'website', align: 'start' }
-        ],
-        search:'',
-        serverItems :[],
-        loading:true,
-        totalItems:0,
-    }),
-    methods:{
-        loadItems({ page ,itemsPerPage}){
             this.loading = true;
 
-            const start = (page-1) * itemsPerPage;
-            const end = start + itemsPerPage;
+            // const start = (page-1) * itemsPerPage;
+            // const end = start + itemsPerPage;
 
-            axios.get('https://jsonplaceholder.typicode.com/users')
-            .then(res => {
-                const allUsers = res.data;
+            axios.get('http://127.0.0.1:8000/api/usersPage', {
+                    params: {
+                        page: page,
+                        per_page: itemsPerPage
+                    }
+                })
+                .then(res => {
+                    // const paginatedUsers = res.data.users.data;
+                    // console.log(res.data.users.total);
 
-                const paginatedUsers = allUsers.slice(start,end);
-
-                this.serverItems = paginatedUsers;
-                this.totalItems = allUsers.length;
-                this.loading = false;
-            })
-            .catch(error => {
-                console.log("Error :",error);
-                this.loading = false;
-            });
+                    this.serverItems = res.data.users.data;
+                    this.totalItems = res.data.users.total;
+                    this.loading = false;
+                })
+                .catch(error => {
+                    console.log("Error :", error);
+                    this.loading = false;
+                });
         },
     },
-    created(){
-        this.loadItems({page:1,itemsPerPage:this.itemsPerPage});
+    created() {
+        this.loadItems({
+            page: 1,
+            itemsPerPage: this.itemsPerPage
+        });
     }
-    }
+}
 </script>
